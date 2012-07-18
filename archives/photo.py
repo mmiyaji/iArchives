ff2aa9e0e3ad3156256484a861c7b5f5bb7c3e38
mmiyaji <ruhenheim1223@gmail.com>
2012-07-18 21:51:23 +0900

    add photo.py

---
+++ archives/photo.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+photo.py
+
+Created by mmiyaji on 2012-07-18.
+Copyright (c) 2012  ruhenheim.org. All rights reserved.
+"""
+from views import *
+
+def detail():
+    """
+    Case of GET REQUEST '/photo/'
+    画像の一覧を表示するページ
+    """
+    
+
+def view():
+    """
+    Case of GET REQUEST '/photo/<photo_uuid>/'
+    個別の画像の詳細を表示するページ
+    """
+
+
+def delete():
+    """
+    Case of DELETE REQUEST '/photo/<photo_uuid>/delete'
+    対象画像の削除
+    DELETE リクエストにのみレスポンス
+    """
+    
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    
+
